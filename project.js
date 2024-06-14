1d7feacc551ff46e65617de6758832df5f81a863
Rihan01-log <rihanrasheed201@gmail.com>
2024-06-14 09:37:49 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=> {
+    menuIconc.classlist.toggle('bx-x')
+    navbar.classlist.toggle('active')
+}
